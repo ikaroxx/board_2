92821f675009bff31f3ddc12772f384bd6ddb467
KONGWONGYU <kwg9790@naver.com>
2023-11-17 17:08:08 +0900

    첨부파일 기초준비

--- src/main/java/kwg/board/dto/BoardDTO.java
+++ src/main/java/kwg/board/dto/BoardDTO.java
@@ -2,6 +2,7 @@ package kwg.board.dto;
 
 import kwg.board.entity.BoardEntity;
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDateTime;
 
@@ -21,6 +22,14 @@ public class BoardDTO {
     private int boardHits;
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
+
+    private MultipartFile boardFile; // save.html -> Controller 파일 담는 용도
+    private String originalFileName; // 파일 파일 이름
+    private String storedFileName;   // 서버 저장용 파일 이름 
+    private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
+
+
+
 
     public BoardDTO(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
--- src/main/java/kwg/board/entity/BoardEntity.java
+++ src/main/java/kwg/board/entity/BoardEntity.java
@@ -31,6 +31,9 @@ public class BoardEntity extends BaseEntity {
     @Column
     private int boardHits;
 
+    @Column
+    private int fileAttached; // 1 or  0
+
     public static BoardEntity toSaveEntity(BoardDTO boardDTO){
         BoardEntity boardEntity = new BoardEntity();
         boardEntity.setBoardWriter(boardDTO.getBoardWriter());
@@ -38,6 +41,7 @@ public class BoardEntity extends BaseEntity {
         boardEntity.setBoardTitle(boardDTO.getBoardTitle());
         boardEntity.setBoardContents(boardDTO.getBoardContents());
         boardEntity.setBoardHits(0);
+        boardEntity.setFileAttached(0); // 파일 없음.
         return boardEntity;
 
     }
